0c90fd5ee33fa2aa0ee992f9ed11652d0d95dd2d
hama2323 <hamamura0723@gmail.com>
2023-03-22 18:40:48 +0900

    課題提出

---
+++ src/main/java/com/techacademy/InputController.java
@@ -0,0 +1,27 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class InputController {
+
+    @GetMapping("/input")
+    public String getForm() {
+     // input.htmlに画面遷移
+        return "input";
+    }
+}
+
+/*    @PostMapping("/output")
+    public String postForm(@RequestParam(name = "val", required = false ) String val, Model model) {
+        // フォームから送信されてきた値をModelに登録
+        model.addAttribute("val",val);
+        // output.htmlに画面遷移
+        return "output";
+
+}
+}*/
